0852b02441955bec0bad93747973ae6a8a2e3da4
Marco Junior <masoaresjr1@gmail.com>
2023-11-02 18:55:57 -0300

    Criar struct para erros default

--- internal/common/errors/default_error.go
+++ internal/common/errors/default_error.go
@@ -1,1 +1,14 @@
 package errors
+
+import "fmt"
+
+// Error default error type
+type Error struct {
+	Context string
+	Message string
+	Err     error
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("Context: %s, %s | Error: %s", e.Context, e.Message, e.Err.Error())
+}
