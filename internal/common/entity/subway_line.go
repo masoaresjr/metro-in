d17f735047ae3ffdff7266a8d8be59af82221150
Marco Junior <masoaresjr1@gmail.com>
2023-11-02 18:56:10 -0300

    Mudanças na formatação

--- internal/common/entity/subway_line.go
+++ internal/common/entity/subway_line.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 // SubwayLine entity
 type SubwayLine struct {
 	gorm.Model
-	Name     string 			 `json:"name" gorm:"text"`
+	Name     string              `json:"name" gorm:"text"`
 	Stations []SubwayLineStation `gorm:"-"`
 }
